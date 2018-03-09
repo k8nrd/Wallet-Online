972b8a8dbe61cc394156826d2a6d91da0f2600fe
k8nrd <konrad.loch1@gmail.com>
2018-03-09 23:54:49 +0100

    Main interface update.

--- wallet-frontend/src/app/app.module.ts
+++ wallet-frontend/src/app/app.module.ts
@@ -4,12 +4,26 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './StartPage/start-page/start-page.component';
+import { RegisterPageComponent } from './StartPage/register-page/register-page.component';
+import { NavbarComponent } from './Navbar/navbar/navbar.component';
+import { MainComponent } from './Main/main/main.component';
+import { NavMenuComponent } from './Main/main/nav-menu/nav-menu.component';
+import { TransactionListComponent } from './Main/main/transaction-list/transaction-list.component';
+import { TransactionAddComponent } from './Main/main/transaction-add/transaction-add.component';
+import { ChartsComponent } from './Main/main/charts/charts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartPageComponent
+    StartPageComponent,
+    RegisterPageComponent,
+    NavbarComponent,
+    MainComponent,
+    NavMenuComponent,
+    TransactionListComponent,
+    TransactionAddComponent,
+    ChartsComponent
   ],
   imports: [
     NgbModule.forRoot(),
